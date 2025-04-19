7d0357802782c0d6b062c6547f525b8546f8f1ae
bobosii <day.emir@hotmail.com>
2025-04-19 22:46:58 +0300

    JwtTokenValidator and AppConfig

---
+++ src/main/java/com/emir/project_management_system/config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.emir.project_management_system.config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY = "adjhfsdnafjwoflqwfhdjinkasdkjasfbajksnd";
+    public static final String JWT_HEADER = "Authorization";
+}
